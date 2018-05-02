420c13d8ca758c53f04ce8c43b426dd7887269b6
KatjaAlbreht <katja.albreht.soncek@gmail.com>
2018-05-02 20:10:50 +0200

    dodane definicije
    potence, koreni in logaritmi

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import math
 
+## OSNOVNE FUNKCIJE ############################################################
+
 def sestevanje(x, y):
     return x + y
 
@@ -11,9 +13,11 @@ def mnozenje(x, y):
 
 def deljenje(x, y):
     if y == 0:
-        return 'Error'
+        return print('Error')
     else:
         return x / y
+
+## ŠTEVILO PI IN e #############################################################
 
 def pi():
     return math.pi 
@@ -21,9 +25,11 @@ def pi():
 def euler():
     return math.exp(1)
 
+## FAKULTETA ###################################################################
+
 def fakulteta(x):
     if x < 0:
-        return 'Error'
+        return print('Error')
     elif x == 0:
         return 1
     elif x == 1:
@@ -33,14 +39,73 @@ def fakulteta(x):
     else:
         return x * fakulteta(x-1)
 
+## DISKRETNA MATEMATIKA ########################################################
+
 def nPr(n, r):
     if n <= 0 or r <= 0 or n < r:
-        return 'Error'
+        return print('Error')
     else:
         return fakulteta(n) / fakulteta(n - r)
 
 def nCr(n, r):
     if n <= 0 or r <= 0 or n < r:
-        return 'Error'
+        return print('Error')
     else:
         return fakulteta(n) / (fakulteta(r) * fakulteta (n - r))
+
+## POTENCE #####################################################################
+
+def kvadrat(x):
+    return x ** 2
+
+def kub(x):
+    return x ** 3
+
+def obratna_vrednost(x):
+    return x ** (-1)
+
+def potenca(x, n):
+    return x ** n
+
+## KORENI ######################################################################
+
+def kvadratni_koren(x):
+    return math.sqrt(x)
+
+def kubicni_koren(x):
+    return x ** (1 / 3)
+
+def koren(x, n):
+    if n == 0:
+        return print('Error')
+    else:
+        return x ** (1 / n)
+
+## LOGARITMI ###################################################################
+
+def logaritem_10(x):
+    return math.log10(x)
+
+def logaritem_e(x):
+    return math.log(x)
+
+def logaritem(x, a):
+    return math.log(x, a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
