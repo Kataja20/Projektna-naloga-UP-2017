0abd3c4f59c89b54063138dca283beaef8529ed2
KatjaAlbreht <katja.albreht.soncek@gmail.com>
2018-05-02 16:31:59 +0200

    dodani definiciji števila e in fakultete

--- model.py
+++ model.py
@@ -17,3 +17,18 @@ def deljenje(x, y):
 
 def pi():
     return math.pi 
+
+def euler():
+    return math.exp(1)
+
+def fakulteta(x):
+    if x < 0:
+        return 'Error'
+    elif x == 0:
+        return 1
+    elif x == 1:
+        return 1
+    elif x == 2:
+        return 2
+    else:
+        return x * fakulteta(x-1)
